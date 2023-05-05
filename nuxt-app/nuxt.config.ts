63167d6be073dd6f41cf58983cf74d7e9bd02d04
이충재 <knm8643@nate.com>
2023-05-05 20:15:51 +0900

    환경설정수정

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    generate: {
-        dir: 'dist'
-    },
+    // generate: {
+    //     dir: 'dist'
+    // },
     target: 'static',
     router: {
         base: '/opensimlitest/'
